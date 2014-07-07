8f96d8a858c32c7a03b77774f883cecdd210a4b3
Quinn Slack <qslack@qslack.com>
2014-07-07 12:48:13 -0700

    initial commit

---
+++ examples/x0.go
@@ -0,0 +1,8 @@
+package main
+
+import "log"
+
+func main() {
+	x := 0
+	log.Printf("hello %d!", x)
+}
---
+++ examples/x1.go
@@ -0,0 +1,8 @@
+package main
+
+import "log"
+
+func main() {
+	x := 1
+	log.Printf("hello %d!", x)
+}
